4fa0a242a40d2a1c10a3f34e244c601039347ae9
Pablo Salgado <pablo.salgado@iungoeducation.com>
2018-05-20 14:16:33 -0500

    Ingreso de usuario. Valida credenciales contra el servidor y carga la actividad principal.

---
+++ app/src/main/java/co/edu/unadvirtual/computacion/movil/AppSingleton.java
@@ -0,0 +1,42 @@
+package co.edu.unadvirtual.computacion.movil;
+
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Módulo Singleton para manejar todas las operaciones de red al servidor de UNADroid. Este módulo
+ * hace uso de la biblioteca Volley que permite simplificar el manejo de las operaciones de red.
+ */
+public class AppSingleton {
+    private static AppSingleton instance;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private AppSingleton(Context context) {
+        AppSingleton.context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized AppSingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new AppSingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req,String tag) {
+        req.setTag(tag);
+        getRequestQueue().add(req);
+    }
+}
---
+++ app/src/main/java/co/edu/unadvirtual/computacion/movil/iam/LoginActivity.java
@@ -0,0 +1,134 @@
+package co.edu.unadvirtual.computacion.movil.iam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import co.edu.unadvirtual.computacion.movil.AppSingleton;
+import co.edu.unadvirtual.computacion.movil.MainActivity;
+import co.edu.unadvirtual.computacion.movil.R;
+
+/**
+ * Captura las credenciales del usuario y las valida contra el servidor
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private EditText editTextPassword;
+    private EditText editTextUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextUsername = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser(
+                        editTextUsername.getText().toString(),
+                        editTextPassword.getText().toString()
+                );
+            }
+        });
+
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    /**
+     * Método para llevar a cabo la autenticación del usuario.
+     *
+     * @param email El nombre de usuario.
+     * @param password La contraseña del usuario.
+     */
+    private void loginUser(final String email, final String password) {
+        // Construir las credenciales del usuario para ser enviadas al servidor en formato JSON
+        JSONObject params = new JSONObject();
+
+        try {
+            params.put("email", email);
+            params.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Se hace una petición POST al servidor con las credenciales de acceso del usuario.
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                "https://unadroid.tk/api/login",
+                params,
+                new SuccessListener(),
+                new ErrorListener()
+        );
+
+        // Enviar la petición a la cola. Los resultados enviados por el servidor serán procesados
+        // por los Listener asociados en el momento que la respuesta arrive al dispositivo.
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(request, TAG);
+    }
+
+    /**
+     * Procesa la respuesta del servidor. Si las credenciales que el usuario ha ingresado permiten
+     * autenticar con éxito, se carga la aplicación
+     */
+    private class SuccessListener implements Response.Listener<JSONObject> {
+        @Override
+        public void onResponse(JSONObject response) {
+            try {
+                boolean error = !response.isNull("error");
+
+                if (!error) {
+                    Intent intent = new Intent(
+                            LoginActivity.this,
+                            MainActivity.class);
+                    intent.putExtra("email", response.getString("email"));
+                    startActivity(intent);
+                    finish();
+                } else {
+                    String errorMsg = response.getString("error_msg");
+                    Toast.makeText(
+                            getApplicationContext(),
+                            errorMsg, Toast.LENGTH_LONG
+                    ).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Si ocurre algún error, se despliega el mensaje del mismo.
+     */
+    private class ErrorListener implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(
+                    LoginActivity.this.getApplicationContext(),
+                    error.getMessage(),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+}
