dc4fbd26eb3fb7e07e219504ee8c2a91570f3a84
Juan Sebastian Calle <jsebascalle@gmail.com>
2018-06-14 22:25:31 -0500

    Se agrega listar temas de unidad

--- app/src/main/java/co/edu/unadvirtual/computacion/movil/common/ListTopicsActivity.java
+++ app/src/main/java/co/edu/unadvirtual/computacion/movil/common/ListTopicsActivity.java
@@ -1,5 +1,6 @@
 package co.edu.unadvirtual.computacion.movil.common;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.design.widget.NavigationView;
@@ -12,6 +13,7 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -40,8 +42,10 @@ public class ListTopicsActivity extends AppCompatActivity  {
 
     private static final String TAG = "ListTopicsActivity";
     private ProgressBar progressBar;
+    private TextView  titleUnit;
     private int unit_id;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +54,8 @@ public class ListTopicsActivity extends AppCompatActivity  {
         Bundle data = intent.getExtras();
         unit_id = data.getInt("MAINACTIVITY_PARAMS_UNIT_ID");
         progressBar = findViewById(R.id.progressBarTopics);
+        titleUnit = findViewById(R.id.titleUnit);
+        titleUnit.setText("Unidad "+unit_id);
         callTopicsList();
     }
 
@@ -115,17 +121,17 @@ public class ListTopicsActivity extends AppCompatActivity  {
             mainTextParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
             subTextParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
             relativeLayout = new RelativeLayout(this);
-            cardParams.setMargins(25, 25, 25, 25);
+            cardParams.setMargins(25, 5, 25, 5);
 
             card = new CardView(getApplicationContext());
             card.setLayoutParams(cardParams);
-            card.setPadding(10, 10, 10, 10);
+            card.setPadding(10, 20, 10, 20);
             card.setId(topic.getId() + 6000);
             card.setCardBackgroundColor(Color.WHITE);
             card.setRadius(7);
 
             card.setOnClickListener(view -> {
-                /*Intent intent = new Intent(MainActivity.this, ListTopicsActivity.class);
+                /*Intent intent = new Intent( ListTopicsActivity.class, ListResourceActivity.this);
                 intent.putExtra("MAINACTIVITY_PARAMS_TOPIC_ID",topic.getId());
                 startActivity(intent);*/
             });
@@ -137,10 +143,11 @@ public class ListTopicsActivity extends AppCompatActivity  {
             imageParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
             imageParams.addRule(RelativeLayout.ALIGN_PARENT_START, RelativeLayout.TRUE);
             imageParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-            imageParams.setMargins(getDpTopic(16), getDpTopic(16), getDpTopic(16), 0);
+            imageParams.setMargins(5, 5, getDpTopic(16), 0);
             imageView.setLayoutParams(imageParams);
             imageView.getLayoutParams().width = getDpTopic(62);
             imageView.getLayoutParams().height = getDpTopic(50);
+            imageView.setImageResource(getResources().getIdentifier("ic_folder", "drawable", getPackageName()));
             relativeLayout.addView(imageView, 0);
 
 
@@ -149,10 +156,10 @@ public class ListTopicsActivity extends AppCompatActivity  {
             mainTextParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
             mainTextParams.addRule(RelativeLayout.END_OF, imageView.getId());
             mainTextParams.addRule(RelativeLayout.RIGHT_OF, imageView.getId());
-            mainTextParams.setMargins(15, 25, 0, 0);
+            mainTextParams.setMargins(10, 10, 0, 10);
             mainText.setLayoutParams(mainTextParams);
             mainText.setText(topic.getName());
-            mainText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+            mainText.setTextSize(18);
             mainText.setTextColor(getResources().getColor(R.color.colorPrimary));
             relativeLayout.addView(mainText, 1);
 
