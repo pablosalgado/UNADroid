725074427c2c5da7db83ebaa353bcb21aa80cb1d
Pablo Salgado <pablo.salgado@iungoeducation.com>
2018-06-19 11:41:27 -0500

    Adiciona recuperación de acceso.

--- app/src/main/java/co/edu/unadvirtual/computacion/movil/ViewResourceActivity.java
+++ app/src/main/java/co/edu/unadvirtual/computacion/movil/ViewResourceActivity.java
@@ -1,6 +1,8 @@
 package co.edu.unadvirtual.computacion.movil;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,52 +25,64 @@ public class ViewResourceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_resource);
 
-        unit_id = getIntent().getIntExtra("ACTIVITY_PARAMS_UNIT_ID",0);
-        topic_id = getIntent().getIntExtra("ACTIVITY_PARAMS_TOPIC_ID",0);
+        unit_id = getIntent().getIntExtra("ACTIVITY_PARAMS_UNIT_ID", 0);
+        topic_id = getIntent().getIntExtra("ACTIVITY_PARAMS_TOPIC_ID", 0);
         resource_url = getIntent().getStringExtra("ACTIVITY_PARAMS_URL");
-        resource_type = getIntent().getIntExtra("ACTIVITY_PARAMS_RESOURCE_TYPE",3);
+        resource_type = getIntent().getIntExtra("ACTIVITY_PARAMS_RESOURCE_TYPE", 3);
 
-        if(resource_type ==1){
-            resource_url = "https://www.youtube.com/embed/" + resource_url + "?autoplay=1&vq=small";
+        if (resource_type == 1) {
+            playVideo(resource_url);
+        } else {
+            //Toast.makeText(getApplicationContext(), resource_url, Toast.LENGTH_LONG).show();
+            WebView webView = findViewById(R.id.resource_webview);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setAppCacheEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setDatabaseEnabled(true);
+            webView.getSettings().setGeolocationEnabled(true);
+            webView.loadUrl(resource_url);
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setPluginState(WebSettings.PluginState.ON);
+            webSettings.setJavaScriptEnabled(true);
+            webView.setWebChromeClient(new WebChromeClient() {
+                @Override
+                public void onReceivedTitle(WebView view, String title) {
+                    getWindow().setTitle(title); //Set Activity tile to page title.
+                }
+
+                @Override
+                public void onGeolocationPermissionsShowPrompt(String origin,
+                                                               GeolocationPermissions.Callback callback) {
+                    callback.invoke(origin, true, false);
+                }
+            });
+
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
         }
-        //Toast.makeText(getApplicationContext(), resource_url, Toast.LENGTH_LONG).show();
-        WebView webView = findViewById(R.id.resource_webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setAppCacheEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setDatabaseEnabled(true);
-        webView.getSettings().setGeolocationEnabled(true);
-        webView.loadUrl(resource_url);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setPluginState(WebSettings.PluginState.ON);
-        webSettings.setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public void onReceivedTitle(WebView view, String title) {
-                getWindow().setTitle(title); //Set Activity tile to page title.
-            }
-
-            @Override
-            public void onGeolocationPermissionsShowPrompt(String origin,
-                                                           GeolocationPermissions.Callback callback) {
-                callback.invoke(origin, true, false);
-            }
-        });
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
     }
 
+    private void playVideo(String resource_url) {
+        try {
+            // Se intenta cargar el video en la app de YouTube, si está instalada
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + resource_url));
+            startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            // Si la aplicación no existe, se carga directamente en el navegador
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + resource_url));
+            startActivity(intent);
+        }
+    }
 
-    public void callStander(View view){
+    public void callStander(View view) {
         Intent intent = new Intent(ViewResourceActivity.this, ListResourcesActivity.class);
-        intent.putExtra("ACTIVITY_PARAMS_UNIT_ID",unit_id);
-        intent.putExtra("ACTIVITY_PARAMS_TOPIC_ID",topic_id);
+        intent.putExtra("ACTIVITY_PARAMS_UNIT_ID", unit_id);
+        intent.putExtra("ACTIVITY_PARAMS_TOPIC_ID", topic_id);
         startActivity(intent);
     }
 }
